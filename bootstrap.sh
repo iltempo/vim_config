cc130273026d58d37065b27f53fb7392d7e489a8
Alexander Greim <alexxx@iltempo.de>
2013-07-14 20:52:25 +0200

    Ensure config directory for bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@ function die()
     exit 1
 }
 
+mkdir -p ${HOME}/.vim_config
 git clone git@github.com:iltempo/vim_config.git ${HOME}/.vim_config
 cd $HOME/.vim_config || die "Could not go into the ${HOME}/.vim_config"
 rake install || die "rake install failed."
