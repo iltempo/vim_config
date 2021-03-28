786daa0ab2df746b1fe1195fa122badfddd32564
Alexander Greim <alexxx@iltempo.de>
2021-03-28 19:00:18 +0200

    Add GnuPG config to fish

---
+++ config/fish/conf.d/gnupg.fish
@@ -0,0 +1,6 @@
+# Start or re-use a gpg-agent
+gpgconf --launch gpg-agent
+
+# Ensure that GPG Agent is used as the SSH agent
+set -e SSH_AUTH_SOCK
+set -U -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
